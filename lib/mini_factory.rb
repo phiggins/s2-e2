424f77b59cb1b64c368421abb1fd4517f1ecbba7
phiggins <pete@peterhiggins.org>
2010-10-16 13:58:53 -0700

    Add ability for factory name to be different from model name.

--- lib/mini_factory.rb
+++ lib/mini_factory.rb
@@ -1,26 +1,38 @@
 class MiniFactory
-  class << self 
-    attr_accessor :factories
+  def self.clear_factories!
+    @factories = nil
+  end
+
+  def self.factories
+    @factories ||= {}
   end
   
-  def self.define name, &block
+  def self.define name, opts={}, &block
     name = name.to_s.downcase.to_sym
-    @factories ||= {}
-    @factories[name] = block
+    factories[name] = new(name, opts, block)
   end
 
   def self.create name
     name = name.to_s.downcase.to_sym
-    model = model(name)
-    proxy = Proxy.new(model.new)
-    @factories[name].call( proxy )
-    record = proxy.target
-    record.save
-    record
+    factory = factories[name]
+    factory.create
   end
 
-  def self.model obj
-    Object.const_get obj.to_s.capitalize
+  def initialize name, opts={}, block
+    @name   = name
+    @block  = block
+    @model  = opts[:class] || model
+  end
+
+  def create
+    real_object = @model.new
+    proxy = Proxy.new(real_object)
+    @block.call(proxy)
+    real_object.save
+  end
+
+  def model
+    Object.const_get @name.to_s.capitalize
   end
 
   class Proxy
--- spec/mini_factory_spec.rb
+++ spec/mini_factory_spec.rb
@@ -7,6 +7,7 @@ DB.create_table :users do
   primary_key :id
   String :first_name
   String :last_name
+  Boolean :admin
 end
 
 class User < Sequel::Model ; end
@@ -16,48 +17,61 @@ MiniTest::Unit.autorun
 
 describe MiniFactory do
   before do
-    # XXX Lame hacks...
-    MiniFactory.factories = {}
+    MiniFactory.clear_factories!
   end
 
   it "should support basic factory_girl use case" do
-    #MiniFactory.define :user do |u|
     MiniFactory.define :user do |u|
       u.first_name 'John'
       u.last_name  'Doe'
+      u.admin false
     end
 
     u = MiniFactory(:user)
     u.first_name.must_equal "John"
     u.last_name.must_equal "Doe"
+    u.admin.must_equal false
   end
 
   describe ".define" do
+    it "should allow specifying the class with :class option" do
+      MiniFactory.define :admin, :class => User do |u|
+        u.first_name 'Admin'
+        u.last_name  'User'
+        u.admin true
+      end
+
+      MiniFactory(:admin).must_be_kind_of User
+    end
+
     it "should accept a model" do
       MiniFactory.define User do |u|
         u.first_name 'John'
         u.last_name  'Doe'
+        u.admin false
       end
 
-      MiniFactory.factories.has_key?(:user).must_equal true
+      MiniFactory(:user).must_be_kind_of User
     end
 
     it "should accept a symbol" do
       MiniFactory.define :user do |u|
         u.first_name 'John'
         u.last_name  'Doe'
+        u.admin false
       end
 
-      MiniFactory.factories.has_key?(:user).must_equal true
+      MiniFactory(:user).must_be_kind_of User
     end
 
     it "should accept a string" do
       MiniFactory.define "user" do |u|
         u.first_name 'John'
         u.last_name  'Doe'
+        u.admin false
       end
 
-      MiniFactory.factories.has_key?(:user).must_equal true
+      MiniFactory(:user).must_be_kind_of User
     end
   end
 end
